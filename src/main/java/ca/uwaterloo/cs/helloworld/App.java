b788b726ff931d70391b7e18e47de89d99cf49d7
Sarah Nadi <nadi@ualberta.ca>
2024-01-21 11:08:11 +0400

    Simplify pom and add external dependency

--- src/main/java/ca/uwaterloo/cs/helloworld/App.java
+++ src/main/java/ca/uwaterloo/cs/helloworld/App.java
@@ -1,5 +1,7 @@
 package ca.uwaterloo.cs.helloworld;
 
+import com.google.common.base.Joiner;
+
 /**
  * Hello world!
  */
@@ -8,5 +10,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello, world!" );
+
+        Joiner joiner = Joiner.on("; ").skipNulls();
+        String newMsg = joiner.join("Hello World", "in CS 3261");
+
+        System.out.println(newMsg);
     }
 }
